3d60317ae2f1545022c992b6e6c6480a5cdf4dc9
Brandon Johnson <brandon121j@gmail.com>
2022-05-06 14:24:16 -0400

    fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var corsOptions = {
   },
   credentials: true
   }
-// app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 // app.options('', cors());
 
 app.use(logger('dev'));
@@ -41,10 +41,10 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  
+
   // res.header("Access-Control-Allow-Origin", origin);
   // res.header("Access-Control-Allow-Credentials", true);
-  
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -52,6 +52,3 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send('error');
 });
-
-
-module.exports = app;
